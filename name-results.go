150fbecd7f84ba07059b4315f54798cc652f7e27
Lekia <lekiagospel@gmail.com>
2020-06-15 00:55:17 +0100

    named results

---
+++ name-results.go
@@ -0,0 +1,15 @@
+// Go's return values may be named. If so, they are treated as variables defined at the top of the function.
+
+package main
+
+import "Fmt"
+
+func split(sum int) (x, y int) {
+	x = sum * 4/9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
