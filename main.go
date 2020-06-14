aec5ca346d1642c52afacd9a2ce6ab053b46e3db
Lekia <lekiagospel@gmail.com>
2020-06-14 21:52:14 +0100

    packages

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
-package main
 
+// Packages:
+// 1. Every Go program is made up of packages. (single import)
+// 2. Programs start running in package main.
+
+package main
 import "fmt"
 
 func main() {
 	fmt.Println("Hello Go")
 }
+
+// on terminal : go run package.go
+
---
+++ package.go
@@ -0,0 +1,13 @@
+// multiple import
+package main 
+
+import ( 
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	fmt.Println("My favourite numebr is ", rand.Intn(10)) // N/B 'P' in Print is uppercase
+}
+// on terminal : go run package.go
