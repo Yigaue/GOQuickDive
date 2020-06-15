66ce32ae3f9152c9007aad42cca2df285c29029f
Lekia <lekiagospel@gmail.com>
2020-06-15 20:28:14 +0100

    variables-with-initializers

---
+++ variables-with-initialisers.go
@@ -0,0 +1,13 @@
+//A var declaration can include initializers, one per variable.
+
+// If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
+
+package main
+
+import "fmt"
+
+var i, name, gender, occupation = 2, "Sonia", "male", "Developer"
+
+func main(){
+	fmt.Println(1, name, gender, occupation)
+}
