e7c9cf74e553e56970c2007ab6bd55285f249ee2
Lekia <lekiagospel@gmail.com>
2020-06-20 20:52:33 +0100

    struct pointers

---
+++ struct-pointers.go
@@ -0,0 +1,19 @@
+// Struct fields can be accessed through a struct pointer.
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
+
+// terminal: go run struct-pointers.go
