d51c27d612929d912c4184894126b640345d8acd
Lekia <lekiagospel@gmail.com>
2020-06-21 21:48:03 +0100

    maps

---
+++ maps.go
@@ -0,0 +1,18 @@
+// A map maps keys to values.
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+}
