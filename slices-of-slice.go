dbba4f8e55a110703efc6f7dc22a083e51722dfb
Lekia <lekiagospel@gmail.com>
2020-06-21 18:54:43 +0100

    slices of slice

---
+++ slices-of-slice.go
@@ -0,0 +1,33 @@
+// Slices can contain any type, including other slices.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The Players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+// terminal: go run slices-of-slice.go
+
+
+
