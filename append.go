f1d06f0af69384da15edec768fd66b9fab8a6b2c
Lekia <lekiagospel@gmail.com>
2020-06-21 19:14:46 +0100

    append slice

---
+++ append.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	// append works nil slices
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// we can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+
+}
+// terminal: go run append.go
