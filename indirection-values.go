8128bd2850260a69198ecf4c96685b7b23aef9f8
Lekia <lekiagospel@gmail.com>
2020-06-23 15:21:57 +0100

    indirection values

---
+++ indirection-values.go
@@ -0,0 +1,32 @@
+// The equivalent thing happens in the reverse direction.
+// Functions that take a value argument must take a value of that specific type:
+// while methods with value receivers take either a value or a pointer as the receiver when they are called:
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
+
+	p := &Vertex{4, 3}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
+}
