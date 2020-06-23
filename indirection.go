abf6740efd00eb329cf5185d22ea0b4f1996ff47
Lekia <lekiagospel@gmail.com>
2020-06-23 11:26:11 +0100

    indirection

---
+++ indirection.go
@@ -0,0 +1,32 @@
+//  Functions with a pointer argument must take a pointer:
+// while methods with pointer receivers take either a value or a pointer as the receiver when they are called:
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)
+}
