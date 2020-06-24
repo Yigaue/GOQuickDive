9a1d5b463950ae4ecca9d934ac4e8ec5077b95a1
Lekia <lekiagospel@gmail.com>
2020-06-24 23:16:09 +0100

    explicit interfaces

---
+++ interfaces-are-satisfied-explicitly.go
@@ -0,0 +1,23 @@
+//A type implements an interface by implementing its methods. There is no explicit declaration of intent, no "implements" keyword.
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+}
